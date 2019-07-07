a7de88115b781aca6bc549bdbdd9b08db7109e52
integerzero <steadyprogrammer@gmail.com>
2019-07-07 17:45:17 +0900

    Interceptor

---
+++ src/main/java/jp/co/me/integerzero/springmvc/WebConfigure.java
@@ -0,0 +1,13 @@
+package jp.co.me.integerzero.springmvc;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfigure implements WebMvcConfigurer {
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new GreetingInterceptor());
+    }
+}
